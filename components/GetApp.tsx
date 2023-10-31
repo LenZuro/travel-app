4ac06e458b1cb81320ccae0ae636f02056fa38f3
LenZuro <126126939+LenZuro@users.noreply.github.com>
2023-10-30 18:31:03 -0700

    Footer added and get app misspelling fixed

--- components/GetApp.tsx
+++ components/GetApp.tsx
@@ -7,7 +7,7 @@ const GetApp = () => {
     <section className="flexCenter w-full flex-col pb-[100px]">
       <div className="get-app">
         <div className="z-20 flex w-full flex-1 flex-col items-start justify-center gap-12">
-          <h2 className="cold-40 lg:bold-64 xl:max-w-[320px]">
+          <h2 className="bold-40 lg:bold-64 xl:max-w-[320px]">
             Get for Free now!
           </h2>
           <p className="regular-16 text-gray-10">
